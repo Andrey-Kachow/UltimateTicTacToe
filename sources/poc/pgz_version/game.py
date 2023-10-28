5a280cce05f40ed0330262939fdb64ef2a76321c
Andrey Popov <ap4220@ic.ac.uk>
2023-10-28 11:14:48 +0100

    Created skeleton game

---
+++ sources/poc/pgz_version/game.py
@@ -0,0 +1,20 @@
+import random
+
+WIDTH = 500
+HEIGHT = 500
+
+white = (255, 255, 255)
+
+game_field = [
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-']
+]
+
+def draw():
+    pass
+
+
+def update():
+    pass
+
