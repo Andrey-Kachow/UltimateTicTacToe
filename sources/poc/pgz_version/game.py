6cee54b3a2dd1b046600c801be65e962e70dd0ec
Andrey-Kachow <andr10xp@gmail.com>
2023-10-28 11:40:46 +0100

    Attempted to calculate required space for a field

--- sources/poc/pgz_version/game.py
+++ sources/poc/pgz_version/game.py
@@ -2,7 +2,7 @@ import random
 
 def create_game_field(level):
     constructor = lambda: create_game_field(level - 1)
-    if level == 0:
+    if level == 1:
         constructor = lambda: '-'
     return [[constructor() for _ in range(3)] for _ in range(3)]
 
@@ -12,10 +12,21 @@ HEIGHT = 500
 
 white = (255, 255, 255)
 
-levels = 2
-print(create_game_field(levels))
+LEVELS = 2
+print(create_game_field(LEVELS))
+
+min_cell_size = 20
+
+grid_width = min_cell_size
+for level in range(1, LEVELS + 1):
+    border = level
+    margin = level
+    grid_width = 3 * grid_width + border * 4 + margin * 2
+    print(grid_width)
+
 
 def draw():
+    #TODO recursively draw field
     pass
 
 
