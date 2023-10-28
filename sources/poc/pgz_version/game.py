f1c04e5913ca32242e82c59603b0f80ed3eaeed4
Andrey-Kachow <andr10xp@gmail.com>
2023-10-28 11:26:12 +0100

    Added test game map constructor

--- sources/poc/pgz_version/game.py
+++ sources/poc/pgz_version/game.py
@@ -1,15 +1,19 @@
 import random
 
+def create_game_field(level):
+    constructor = lambda: create_game_field(level - 1)
+    if level == 0:
+        constructor = lambda: '-'
+    return [[constructor() for _ in range(3)] for _ in range(3)]
+
+
 WIDTH = 500
 HEIGHT = 500
 
 white = (255, 255, 255)
 
-game_field = [
-    ['-', '-', '-'],
-    ['-', '-', '-'],
-    ['-', '-', '-']
-]
+levels = 2
+print(create_game_field(levels))
 
 def draw():
     pass
@@ -17,4 +21,3 @@ def draw():
 
 def update():
     pass
-
