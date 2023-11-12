991d6f5e415cfc74a912fd30b43aa51b9eb919fa
Andrey-Kachow <andr10xp@gmail.com>
2023-11-12 15:42:30 +0000

    Implemented drawing a field of nth order

--- sources/poc/pgz_version/game.py
+++ sources/poc/pgz_version/game.py
@@ -1,4 +1,10 @@
 import random
+import sys
+
+
+border_color = (0, 0, 0)
+margin_color = (200, 200, 200)
+background_color = (255, 255, 255)
 
 def create_game_field(level):
     constructor = lambda: create_game_field(level - 1)
@@ -7,12 +13,12 @@ def create_game_field(level):
     return [[constructor() for _ in range(3)] for _ in range(3)]
 
 
-WIDTH = 500
-HEIGHT = 500
+WIDTH = 1000
+HEIGHT = 1000
 
 white = (255, 255, 255)
 
-LEVELS = 2
+LEVELS = 3
 print(create_game_field(LEVELS))
 
 min_cell_size = 20
@@ -25,8 +31,36 @@ for level in range(1, LEVELS + 1):
     print(grid_width)
 
 
+def draw_field(x, y, allocated_size, margin, border, level):
+    if level == 0:
+        return
+    screen.draw.filled_rect(Rect((x, y), (allocated_size, allocated_size)), color=border_color)
+    
+    subcell_size = (allocated_size - border * 4 - 2 * margin) / 3
+
+    for i in range(3):
+        for j in range(3):
+            
+            sq_pos_x = x + border + margin + (subcell_size + border) * i
+            sq_pos_y = y + border + margin + (subcell_size + border) * j
+
+            screen.draw.filled_rect(
+                Rect((sq_pos_x, sq_pos_y), (subcell_size, subcell_size)),
+                color=margin_color    
+            )
+            draw_field(
+                sq_pos_x + margin - 1,
+                sq_pos_y + margin - 1,
+                subcell_size - 2 * (margin - 1),
+                border - 1,
+                margin - 1,
+                level - 1
+            )
+
+
 def draw():
-    #TODO recursively draw field
+    screen.fill(background_color)
+    draw_field(WIDTH / 2 - grid_width / 2, HEIGHT / 2 - grid_width / 2, grid_width, LEVELS, LEVELS, LEVELS)
     pass
 
 
